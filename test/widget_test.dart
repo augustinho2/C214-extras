d15db9649ce3154cc198d03a171048725fb731a4
augustinho2 <augustocapeto@hotmail.com>
2021-12-13 13:11:19 -0300

    starting an quiz app with two tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,40 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:exercicios/main.dart';
+import 'package:exercicios/question.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  int _questionNumber = 0;
+  const index = 0;
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  int totalCorrect = 0;
+  int totalQuestions = 0;
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+  List<Question> _questions = [
+    Question('Brasilia is the capital of Brazil', true),
+    Question('The Indian Ocean is the third largest ocean in the world', true),
+    Question('The city of Utrecht is in Holland', false),
+    Question('There are more countries in Africa than Asia', true),
+    Question('Seattle is more northerly than New York', true),
+  ];
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  int questionsLength() {
+    return _questions.length;
+  }
+
+  bool isFinshed() {
+    if (_questionNumber >= _questions.length - 1) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  test("Teste do tamanho da quantidade de questões", () {
+    expect(questionsLength(), 5);
+  });
+
+  test("Teste de conclusão", () {
+    expect(isFinshed(), false);
   });
 }
